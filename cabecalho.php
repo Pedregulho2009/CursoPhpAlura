43db112c462d26222ccb82f42535c9445ab3727a
Pedregulho <gomes.pedro2009@gmail.com>
2019-02-27 08:40:52 -0400

    Primeiro commit

---
+++ cabecalho.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Minha loja</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/loja.css" rel="stylesheet" />
+    <script src="main.js"></script>
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">Minha Loja</a>
+        </div>
+        <div>
+            <ul class="nav navbar-nav">
+                <li><a href="produto-formulario.php">Adiciona Produto</a></li>
+                <li><a href="produto-lista.php">Lista de Produtos</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+            </ul>
+        </div>
+    </div><!-- container acaba aqui -->
+</div>
+<div class="container">
+    <div class="principal">
---
+++ produto-formulario.php
@@ -0,0 +1,54 @@
+<?php 
+include("cabecalho.php"); 
+include("conecta.php");
+include("banco-categoria.php");
+
+$categorias = listaCategorias($conexao);
+
+?>
+
+    <h1>Formulário de cadastro</h1>
+    <form action="adiciona-produto.php" method="post">
+        <table class="table">
+            <tr>
+                <td>Nome</td>
+                <td><input type="text" name="nome" /></td>
+            </tr>
+
+            <tr>
+                <td>Preço</td>
+                <td><input type="number" name="preco" /></td>
+            </tr>
+
+            <tr>
+                <td>Descrição</td>
+                <td><textarea name="descricao" class="form-control"></textarea></td>
+            </tr>
+
+            <tr>
+            <tr>
+                <td></td>
+                <td><input type="checkbox" name="usado" value="true"> Usado</td>
+            </tr>
+            <tr>    
+                <td>Categoria</td>
+                <td>
+                    <select name="categoria_id">
+                        <?php foreach($categorias as $categoria) : ?>
+                        <option value="<?=$categoria['id']?>"><?=$categoria['nome']?></option>
+                        <?php endforeach ?>
+                    </select>
+                </td>
+            </tr>
+
+            </tr>
+
+            <tr>
+                <td><input type="submit" value="Cadastrar" /></td>
+            </tr>
+
+        </table>
+
+    </form>
+
+<?php include("rodape.php"); ?>
